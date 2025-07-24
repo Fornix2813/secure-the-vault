3b5f9ddd81a6963913bc70e9d33b03d75a6271b2
Jose Gomez <pepguzman300@gmail.com>
2025-07-23 21:06:29 -0500

    done

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let code2 = (40 - 40) + 80 / 2
 let code3 = 79 % 40
 let codeA = code1 + "-" + code2 + "-" + code3
 let codeB = `${code1}-${code2}-${code3}`
-let message = ("The Vault has been secured. The combination is", codeA)
-console.log(codeA,codeA)
+let message = ("The Vault has been secured. The combination is")
+console.log(message, codeA, "or", codeB)
