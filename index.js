b229a68ab7059aa437e6ab2fdb65238bb6b65793
Jose Gomez <pepguzman300@gmail.com>
2025-07-23 21:03:36 -0500

    JS Final touch

---
+++ index.js
@@ -0,0 +1,7 @@
+let code1 = 3 + 7;
+let code2 = (40 - 40) + 80 / 2
+let code3 = 79 % 40
+let codeA = code1 + "-" + code2 + "-" + code3
+let codeB = `${code1}-${code2}-${code3}`
+let message = ("The Vault has been secured. The combination is", codeA)
+console.log(codeA,codeA)
